b6ece9dd068e29033019941584d6751399b68501
Jameson Allen (Spdermn02) <jameson.f.allen@gmail.com>
2021-06-02 21:24:21 -0500

    Prepping for release updating readme and adding in new images Building new package of .tpp and the .exe

--- README.md
+++ README.md
@@ -1,2 +1,116 @@
-# TouchPortal_Clownfish_VoiceChanger_Plugin
-Interact with Clownfish Voice Changer software
+# Touch Portal Clownfish Voice Changer Plugin
+
+- [Touch Portal Clownfish Voice Changer Plugin](#touch-portal-clownfish-voice-changer-plugin)
+  - [Changelog](#changelog)
+  - [Description](#description)
+  - [Install](#install)
+  - [Actions](#actions)
+    - [Play Audio](#play-audio)
+    - [Text To Speech](#text-to-speech)
+    - [Enable/Disable Clownfish](#enabledisable-clownfish)
+    - [Set Voice Changer](#set-voice-changer)
+    - [Set Custom Pitch](#set-custom-pitch)
+    - [Set Sound FX](#set-sound-fx)
+  - [States](#states)
+    - [Clownfish Voice Selected](#clownfish-voice-selected)
+    - [Clownfish Sound FX Selected](#clownfish-sound-fx-selected)
+  - [Example Page](#example-page)
+  - [Issues](#issues)
+  - [Author](#author)
+  - [License](#license)
+
+## Changelog
+```
+1.0.0 - The initial release
+    - Actions
+        - Play Audio - Choose a sound to play through Clownfish
+        - Text To Speech - Send text to be spoken by Clownfish (Tested only with built in windows voices)
+        - Enable/Disable Clownfish - Turn on or Off Clownfish
+        - Set Voice Changer - Set your Voice Change effect (or set to None to turn off)
+        - Set Custom Pitch - Set a Custom Pitch between -15.0 and 15.0
+        - Set Sound FX - Set your Sound FX (or set to None to turn off)
+    - States
+      Note: These states will only be updated when an action from this plugin fires, and not if changed directly in Clownfish
+        - Clownfish Voice Selected - will be one of the following: None, Alien, Atari, Clone, Mutation, Fast Mutation, Slow Mutation, Male Pitch, Female Pitch, Helium Pitch, Baby Pitch, Radio, Robot, Custom Picth, Silence
+        - Clownfish Sound FX Selected - will be one of the following: None, Church, Cave, Choir, Ghost
+    - Events
+        None
+```
+
+## Description
+If you use Clownfish VoiceChanger this plugin will allow you to change the configuration using Touch Portal! 
+Do you want Text-To-Speech on Windows for FREE, this plugin will do that for you as well.
+
+The downside is there is NO communication back from Clownfish so, it will not be able to stay in sync if you interact directly through Clownfish.  This is a limitation of the API itself and not something that I can fix with the plugin.
+
+## Install
+
+1. Make sure that Clownfish is already started
+2. Import the .tpp file into Touch Portal
+3. Click Okay and Trust Always
+4. Import the sample page if you want
+
+## Actions
+
+![Action Image](resources/images/TPClownfish-Actions.png)
+
+### Play Audio
+
+Play an audio file through Clownfish and it's default outbound device (good for playing direct through microphone)
+
+![Play Audio](resources/images/TPClownfish-PlayAudio.png)
+
+### Text To Speech
+
+Send text you want spoken out to your users and it's default outbound device (good for playing direct through microphone)
+
+![Text To Speech](resources/images/TPClownfish-TextToSpeech.png)
+
+### Enable/Disable Clownfish
+
+Just as the action says, Enable or Disable Clownfish
+
+![Enable Disable](resources/images/TPClownfish-EnableDisable.png)
+
+### Set Voice Changer
+
+Set your selected voice changer option or unset it
+
+![Voice Changer](resources/images/TPClownfish-VoiceChange.png)
+
+### Set Custom Pitch
+
+Set your voice changer to a custom pitch. Allowed values are -15.0 and 15.0
+
+![Custom Pitch](resources/images/TPClownfish-CustomPitch.png)
+### Set Sound FX
+
+Set Sound FX to add on top of your voice change or not.
+
+![Sound FX](resources/images/TPClownfish-SoundFX.png)
+
+## States
+
+### Clownfish Voice Selected
+
+**Valid Values**: None, Alien, Atari, Clone, Mutation, Fast Mutation, Slow Mutation, Male Pitch, Female Pitch, Helium Pitch, Baby Pitch, Radio, Robot, Custom Picth, Silence
+### Clownfish Sound FX Selected
+
+**Valid Values**: None, Church, Cave, Choir, Ghost
+
+## Example Page
+
+[Download](resources/Clownfish-VC-Example.tpz)
+
+*Note: This page uses the Edge Graphics Upgrade which is an additional purchase from Touch Portal, if you don't have it, the icons and gradient backgrounds will still display but the white circle edge will not.*
+
+![Clownfish Page](resources/images/Sample-Page.gif)
+
+## Issues
+Please report issues on the github Issues tab, or in the #clownfish channel on the Official Touch Portal Discord.
+
+## Author
+Jameson Allen (aka Spdermn02)
+
+## License
+MIT see the [LICENSE](LICENSE) file
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
+// Info sourced from: https://clownfish-translator.com/voicechanger/#clownfishAPI
+
 const ref = require('ref-napi');
+const fs = require('fs');
 const process = require('process');
 const Struct = require('ref-struct-di')(ref);
 const w32API = require('win32-api');
 const win32Def = require('win32-def');
 const user32 = w32API.U.load();
 const DS = win32Def.DStruct;
+const TPClient = new (require('touchportal-api').Client)();
+const pluginId = 'TouchPortal_Clownfish_VoiceChanger';
 
 const lpszClass = Buffer.from('CLOWNFISHVOICECHANGER\0','ucs2');
 const hWnd = user32.FindWindowExW(0,0,lpszClass,ref.NULL);
@@ -13,15 +18,194 @@ if (hWnd == undefined || hWnd == null) {
     logIt('ERROR','Could Not find Clownfish Voice Changer running');
     process.exit(1);
 }
-sendMessage("1|We are working now!");
 
+const COMMAND_LIST = {
+    "Play Audio"        : 0,
+    "Text-to-Speech"    : 1,
+    "Toggle Clownfish"  : 2,
+    "Set Voice Changer" : 3,
+    "Set Sound FX"      : 4,
+    "Set Sound Volume"  : 5
+    /* "VST Effect"        : 6,
+    "Toggle Music"      : 7 */
+};
+
+const VOICE_LIST = {
+    "None"          : 0,
+    "Alien"         : 1,
+    "Atari"         : 2,
+    "Clone"         : 3,
+    "Mutation"      : 4,
+    "Fast Mutation" : 5,
+    "Slow Mutation" : 6,
+    "Male Pitch"    : 7,
+    "Female Pitch"  : 8,
+    "Helium Pitch"  : 9,
+    "Baby Pitch"    :10,
+    "Radio"         :11,
+    "Robot"         :12,
+    "Custom Picth"  :13,
+    "Silence"       :14
+};
+
+const SOUND_FX = {
+    "None"    : 0,
+    "Church" : 1,
+    "Cave"   : 2,
+    "Choir"  : 3,
+    "Ghost"  : 4
+}
+
+const playAudio = (tpmessage) => {
+    let file = tpmessage.data[0].value;
+    if( file !== '' || file != undefined || file != null ) {
+        let allowed = false;
+        //check if file exists, else write error
+        try{
+            if( fs.existsSync(file) ) 
+            {
+                allowed = true;
+            }
+        }
+        catch  (err) {
+            logIt('ERROR',`File ${file} caused an error ${err}`);
+            return;
+        }
+        if( !allowed ){
+            logIt('WARN',`File ${file} cannot be used, or is not found`);
+            return;
+        }
+        file = file.replace(/\\/g,'\\\\');
+    }
+    else {
+        file = '';
+    }
+    const message = COMMAND_LIST["Play Audio"] +"|"+ file;
+    console.log(message);
+    sendMessage(message);
+}
+
+const setVoiceChanger = (tpmessage) => {
+    let voiceSelection = VOICE_LIST[tpmessage.data[0].value];
+    if( voiceSelection < 0 || voiceSelection > 14 ) {
+        voiceSelection = 0;
+    }
+    const message = COMMAND_LIST["Set Voice Changer"] +"|"+ voiceSelection;
+    sendMessage(message);
+
+    TPClient.stateUpdate('clownfish_voice_selected', tpmessage.data[0].value);
+}
+
+const setCustomPitch = (tpmessage) => {
+    let pitch = tpmessage.data[0].value;
+    if( pitch < -15.0 || pitch > 15.0 ) {
+        pitch = 0.0;
+    }
+    const message = COMMAND_LIST["Set Voice Changer"] +"|13|"+ pitch;
+    sendMessage(message);
+    TPClient.stateUpdate('clownfish_voice_selected', 'Custom Pitch');
+}
+
+const setSoundFx = (tpmessage) => {
+    let soundFx = SOUND_FX[tpmessage.data[0].value];
+    if( soundFx < 0 || soundFx > 4) {
+        soundFx = 0;
+    }
+    const message = COMMAND_LIST["Set Sound FX"] +"|"+ soundFx;
+    sendMessage(message);
+
+    TPClient.stateUpdate('clownfish_sound_fx_selected',tpmessage.data[0].value);
+}
+
+const textToSpeech = (tpmessage) => {
+    let sayIt = tpmessage.data[0].value;
+    if( sayIt === '' || sayIt == undefined || sayIt == null ) {
+        logIt('WARN','Attempting to use text-to-speech with no text');
+        return;
+    }
+    const message = COMMAND_LIST["Text-to-Speech"] +"|"+ sayIt;
+
+    logIt("DEBUG",`Text to speech message ${message}`)
+    sendMessage(message);
+}
+
+const toggleClownfish = (tpmessage) => {
+    let toggle = (tpmessage.data[0].value === "Enable") ? 0 : 1;
+    const message = COMMAND_LIST["Toggle Clownfish"] +"|"+ toggle;
+    sendMessage(message);
+}
+
+/* const setVST = (tpmessage) => {
+    const vst = tpmessage.data[0].value;
+    const message = COMMAND_LIST["VST Effect"] + "|"+ vst;
+    sendMessage(message);
+}
+
+const setVSTOff = () => {
+    const message = COMMAND_LIST["VST Effect"] + "|disable";
+    sendMessage(message);
+}
+
+const resetVST = () => {
+    const message = COMMAND_LIST["VST Effect"] + "|configure";
+    sendMessage(message);
+} */
 
 function sendMessage(message){
     const copyData = new Struct(DS.COPYDATASTRUCT)()
     copyData.dwData = 42
     copyData.cbData = message.length;
     copyData.lpData = Buffer.from(message+'\0','utf-8');
-    console.log(JSON.stringify(copyData));
+    //console.log(JSON.stringify(copyData));
     const copyDataAddr = ref.address(copyData.ref())
     return user32.SendMessageW(hWnd,0x4A,0,copyDataAddr);
-  }
+}
+
+TPClient.on("Action", (message,hold) => {
+    console.log(pluginId, ": DEBUG : ACTION ", JSON.stringify(message), "hold", hold);
+    switch(message.actionId) {
+        case "clownfish_play_audio":
+            playAudio(message);
+            break;
+        case "clownfish_text_to_speech":
+            textToSpeech(message);
+            break;
+        case "clownfish_toggle":
+            toggleClownfish(message);
+            break;
+        case "clownfish_set_voice_changer":
+            setVoiceChanger(message);
+            break;
+        case "clownfish_set_custom_pitch":
+            setCustomPitch(message);
+            break;
+        case "clownfish_set_sound_fx":
+            setSoundFx(message);
+            break;
+        /* case "clownfish_set_vst":
+            setVST(message);
+            break;
+        case "clownfish_set_vst_off":
+            setVSTOff();
+            break;
+        case "clownfish_reset_vst":
+            resetVST();
+            break; */
+        default:
+            logIt('WARN',`Unknown action of ${message.actionId}`);
+    }
+});  
+
+TPClient.on("Info", (data) => {
+    //TP Is connected now
+    logIt('DEBUG','We are connected, received Info message');
+});
+
+function logIt() {
+    var curTime = new Date().toISOString();
+    var message = [...arguments];
+    var type = message.shift();
+    console.log(curTime,":",pluginId,":"+type+":",message.join(" "));
+}
+    
+TPClient.connect({ pluginId });
